1a85b477641f3804e036484e9366200d0b5a4db8
c3ypt1c <c3ypt1c@gmail.com>
2016-10-30 15:11:48 +0000

    I'll fix this later

--- engine.py
+++ engine.py
@@ -1,2 +1,25 @@
 ##Python compress
 
+fp = "testFile.py";
+f = open ( fp, "rb" );
+fd = f.read();
+f.close();
+del f;
+fd = fd.decode();
+fd = fd.split("\r\n");
+i = 0;
+usualIndent = False;
+
+while i != len(fd)-1:
+    indent = 0;
+    for x in fd[i]:
+        if x == " ":
+            indent += 1;
+        else:
+            break;
+    
+    fd[i] = str ( indent ) + fd[i][indent:];
+    i += 1;
+    
+for x in fd:
+    print ( x );
